c1ecaa83fdfc074b1756d510ab534d927e31fd33
Nehal Bhandari <nehalvbhandari@gmail.com>
2023-12-25 15:06:35 +0530

    test: camera component

--- components/Camera.js
+++ components/Camera.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 const Camera = () => {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const initCamera = async () => {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({
+                    video: true,
+                });
+
+                if (videoRef.current) {
+                    videoRef.current.srcObject = stream;
+                }
+            } catch (error) {
+                console.error("Error accessing camera:", error);
+            }
+        };
+
+        initCamera();
+
+        return () => {
+            // Cleanup: Stop the camera stream when the component is unmounted
+            if (videoRef.current) {
+                const stream = videoRef.current.srcObject;
+                if (stream) {
+                    const tracks = stream.getTracks();
+                    tracks.forEach((track) => track.stop());
+                }
+            }
+        };
+    }, []);
+
     return (
         <div>
-            <h1 className="text-3xl font-bold underline text-white">
-                Hello world!
-            </h1>
+            <h2>Camera Component</h2>
+            <video ref={videoRef} autoPlay playsInline />
         </div>
     );
 };
