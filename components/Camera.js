d1fd0cb597d59e61f37275bfd3c9e4692727ccb2
Nehal Bhandari <nehalvbhandari@gmail.com>
2023-12-25 14:44:10 +0530

    setup: initial commit

---
+++ components/Camera.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Camera = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline text-white">
+                Hello world!
+            </h1>
+        </div>
+    );
+};
+
+export default Camera;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Camera from "../components/Camera";
+
+const Home = () => {
+    return (
+        <div className="bg-black h-screen w-screen">
+            <Camera />
+        </div>
+    );
+};
+
+export default Home;
