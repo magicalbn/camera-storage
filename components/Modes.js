13dde32cffad5a604206e1c8575f3ef51bd31a5e
Nehal Bhandari <nehalvbhandari@gmail.com>
2023-12-26 11:03:31 +0530

    feat: picture modes

--- components/Modes.js
+++ components/Modes.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const Modes = ({ mode, setMode }) => {
+    const containerRef = useRef();
+
+    useEffect(() => {
+        // Scroll to center the active mode when it changes
+        if (containerRef.current && mode) {
+            const containerWidth = containerRef.current.clientWidth;
+            const activeElement = containerRef.current.querySelector(
+                `[data-mode="${mode}"]`
+            );
+
+            if (activeElement) {
+                const scrollLeft =
+                    activeElement.offsetLeft -
+                    containerWidth / 2 +
+                    activeElement.clientWidth / 2 -
+                    17;
+                containerRef.current.scrollTo({
+                    left: scrollLeft,
+                    behavior: "smooth",
+                });
+            }
+        }
+    }, [mode]);
+
     const modesList = [
         "TIME-LAPSE",
         "SLO-MO",
@@ -11,12 +35,16 @@ const Modes = ({ mode, setMode }) => {
     ];
     return (
         <div className="bg-black mt-4">
-            <div className="flex gap-10 overflow-x-auto scrollbar-hide">
+            <div
+                className="flex gap-10 overflow-x-auto scrollbar-hide px-[11rem]"
+                ref={containerRef}
+            >
                 {modesList.map((each) => {
                     return (
                         <p
-                            className={`text-white whitespace-nowrap text-[12px] font-[800] ${
-                                mode == each && "text-[#ffcc00]"
+                            data-mode={each}
+                            className={` whitespace-nowrap text-[12px] font-[800] ${
+                                mode == each ? "text-[#ffcc00]" : "text-white"
                             }`}
                             key={each}
                             onClick={() => setMode(each)}
